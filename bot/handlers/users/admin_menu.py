2d6caa102edb4d139a98168684947ad003cde6c6
SOM <som_msk@promavto.net>
2023-02-27 00:51:47 +0300

    Changed paths

--- bot/handlers/users/admin_menu.py
+++ bot/handlers/users/admin_menu.py
@@ -95,9 +95,17 @@ async def _take_picture(message: Message):
     @dp.message_handler(i18n_text='opencv_show_last', is_admin=True)
     @dp.message_handler(commands=['opencv_show_last'], is_admin=True)
     async def _opencv_show_last(message: Message):
-        await message.reply_photo(open(f'{os.getcwd()}/pictures/last_frame.jpg', 'rb'))
+        path = "opencv"
+        isExist = os.path.exists(path)
+        if not isExist:
+            os.makedirs(path)
+        await message.reply_photo(open(f'{os.getcwd()}/opencv/last_frame.jpg', 'rb'))
 
     @dp.message_handler(i18n_text='opencv_show_last_movement', is_admin=True)
     @dp.message_handler(commands=['opencv_show_last_movement'], is_admin=True)
     async def _opencv_show_last_movement(message: Message):
+        path = "opencv"
+        isExist = os.path.exists(path)
+        if not isExist:
+            os.makedirs(path)
         await message.reply_photo(open(f'{os.getcwd()}/opencv/last_motion_detected.jpg', 'rb'))
